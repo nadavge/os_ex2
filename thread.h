3257af70d5abcd8d8e39ab3017283750c128758a
Nadav <nadav1994@gmail.com>
2015-04-11 19:58:45 +0300

    Update thread to version by Nadav from SwitchScheduler

--- thread.h
+++ thread.h
@@ -1,8 +1,21 @@
+#ifndef _THREAD_H
+#define _THREAD_H
 
+#include <setjmp.h>
+
+/**
+* @brief A struct to represent a user-thread
+*/
 struct Thread {
+	// The threads ID
 	int tid;
 	Priority priority;
-	address_t entry;
+	// Quantums received
 	int quantums;
+	// Holds the environment for the current thread
+	sigjmp_buf env;
+	// The processes stack
 	char stack[STACK_SIZE];
 };
+
+#endif //_THREAD_H
