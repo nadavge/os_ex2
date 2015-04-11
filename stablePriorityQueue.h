9cf8fd71d291beaf631dfad2369692ea8520b144
TheAmazingRabbit <danondaniel@gmail.com>
2015-04-11 19:10:14 +0200

    Added documentation for stable priority

--- stablePriorityQueue.h
+++ stablePriorityQueue.h
@@ -3,15 +3,40 @@
 #include "thread.h"
 using namespace std;
 
-class StablePriorityQueue 
+class StablePriorityQueue
 {
 	public:
+		/**
+		* @brief constructor - initializes the queue
+		**/
 		stablePriorityQueue();
+		/**
+		* @brief adds a thread to the queue
+		* @param thread - pointer to the thread
+		**/
 		void addThread(Thread *thread);
+		/**
+		* @brief removes a thread by pointer from the queue
+		* @param thread - pointer to the thread
+		* @return true on success, false if not or not found
+		**/
 		bool removeThread(Thread *thread);
+		/**
+		* @brief gets a thread by its id
+		* @param id - thread's id
+		* @return pointer to the thread
+		**/
 		Thread* getThreadById(int id);
+		/**
+		* @brief gets the thread with the highest priority
+		* @return pointer to the thread
+		**/
 		Thread* getTopThread();
-		
+
+
+		/**
+		* @brief destructor - frees everything that's needed
+		**/
 		~stablePriorityQueue();
 	private:
 		vector<Thread*> threadQueues[3];
