29bde2524b8ba1282f4fcbf29e0b1398b956a503
TheAmazingRabbit <danondaniel@gmail.com>
2015-04-11 19:16:14 +0200

    Add function skeletons

---
+++ stablePriorityQueue.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include "uthreads.h"
+#include "thread.h"
+
+StablePriorityQueue::StablePriorityQueue()
+{
+
+}
+
+void StablePriorityQueue::addThread(Thread *thread);
+{
+
+}
+
+void StablePriorityQueue::removeThread(Thread *thread);
+{
+
+}
+
+void StablePriorityQueue::getThreadById(int tid);
+{
+
+}
+
+Thread* StablePriorityQueue::getTopThread();
+{
+
+}
+
